45b87f76a36fadce80871800361ffc4d7e4abcb1
ozkancondek <ozkncndek@gmail.com>
2021-08-19 05:02:51 +0200

    cosole

---
+++ my_projects/console/app.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+console.log(3507);
+console.log(true);
+
+var a = 20;
+
+console.log(a);
+console.log(["ozkan","ozkan1"]);
+console.log(typeof(a));
+
+console.warn("Attention");
+console.error("This is an error");
+console.clear(); // clears all terminal
+
+
+
+
+
+
+
+
+
+
