8ccbc24e34f961bd591622efade2741eca11abf3
ozkancondek <ozkncndek@gmail.com>
2021-08-22 17:21:42 +0200

    Variables

---
+++ my_projects/var-let-const/app.js
@@ -0,0 +1,37 @@
+
+//var
+var a = "ozkan";
+var a = 20;//a = 20 -- i can define it again
+console.log(a)
+var a = 45;
+console.log(a);//45
+
+
+//let
+let b = "ozkan";
+//let b = 56 // identifier b has aldready declared--error--
+console.log(b);
+
+
+let x,y;
+x = 10;
+y = 20;
+console.log(x+y);
+
+//const
+
+const e = "ozkan";
+//const e = 22 identifier e has aldready declared-
+console.log(e);
+/*
+const v; i can not write directly like that
+v = 85;   i have to write const v = 85;
+console.log(v)
+*/
+const k =[1,2,3,4];
+console.log(k);
+
+//k = [1,1,2,3] i cant write like that
+//console.log(k)
+k.push(9); // but i can push
+console.log(k);
