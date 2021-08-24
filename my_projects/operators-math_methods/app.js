7bee3e0edd048b2a31ddb5a0abc76291807fcc3a
ozkancondek <ozkncndek@gmail.com>
2021-08-24 15:19:18 +0200

    operators, math methods

---
+++ my_projects/operators-math_methods/app.js
@@ -0,0 +1,28 @@
+let value;
+const value1 = 10;
+const value2 = 4;
+
+//Aritmatic operators
+value = value1 + value2
+value = value1 - value2
+value = value1 * value2
+value = value1 / value2
+value = value1 % value2
+// Math Methods
+value = Math.PI;  // get pi from math
+value = Math.E; // logarithm
+value = Math.round(3.5); // round the values which are equal and bigger than .5 to top limit.
+value = Math.ceil(3.2); // round to top limit
+value = Math.floor(3.8);  // goes to bottom limit
+value = Math.sqrt(64); // root square function
+value = Math.abs(-85); // absolute value function
+value = Math.pow(8,3); // power function
+value = Math.max(1,-9,8,4,-52,3,0,4); // find max num
+value = Math.min(1,4,-6,0,5,78,4,5,5); // min value
+value = Math.random(4,5); // creates between 0 and 1, whatever the limits determined.
+value = Math.floor(Math.random() * (31))+50; // between 50 and 80. Than floor it
+//Math.floor(Math.random() * (max - min+1) ) + min;
+
+
+
+console.log(value);
