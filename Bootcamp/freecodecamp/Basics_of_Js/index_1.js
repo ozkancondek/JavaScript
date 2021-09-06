c0a2b3c939d1a303ad7ceabf0249a175c5807abf
ozkancondek <ozkncndek@gmail.com>
2021-09-06 22:45:12 +0200

    index 1 file

---
+++ Bootcamp/freecodecamp/Basics_of_Js/index_1.js
@@ -0,0 +1,32 @@
+ //Decleration of JavaScript Variables
+ /*JavaScript provides eight different data types which are 
+ -undefined, 
+ -null, 
+ -boolean, 
+ -string, 
+ -symbol, 
+ -bigint, 
+ -number, 
+ and object. */
+
+
+var myVar;
+myVar = 3507;
+var myNum = myVar;
+console.log(myNum);
+
+var sampleStr = "Alan said, \"Peter is learning JavaScript\"."; // use of backslash(\) its ignores the quote
+ 
+var conversation = 'Finn exclaims to Jake, "Algebraic!"'; 
+/*   escape sequences 
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
+ */
+
+ 
