e91f82153feb139fe106c4b93d6153707bd9fece
ozkancondek <ozkncndek@gmail.com>
2021-09-07 19:19:20 +0200

    index_2 file

---
+++ Bootcamp/freecodecamp/Basics_of_Js/index_2.js
@@ -0,0 +1,13 @@
+//Concatenating Strings
+
+var a = "ozkan";  // I can not say that a[0] == "l"
+var b = "condek";
+var c;
+c = a+b;
+c += " is my name."
+c += b
+c = c + " and Iam."
+console.log(c);
+console.log("ozkan".length);
+console.log("ozkan"[0]);
+console.log(a[a.length - 1]); // last character
