a5b918ea2c611f669e72a7b817b5cc5463828c18
ozkancondek <ozkncndek@gmail.com>
2021-09-08 21:52:53 +0200

    minute to second example added

---
+++ myProjects/minutes_to_seconds.js
@@ -0,0 +1,12 @@
+
+
+//Write a function that takes an integer minutes and converts it to seconds.
+
+ 
+ 
+function minToSecond(min){
+    sec = min * 60;
+    return sec;
+};
+
+console.log(minToSecond(3));
