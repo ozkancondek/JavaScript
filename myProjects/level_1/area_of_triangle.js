830f39163de5999fb37b910a1d4e0b0c41083e14
ozkancondek <ozkncndek@gmail.com>
2021-09-08 22:01:23 +0200

    area of triangle example added

---
+++ myProjects/level_1/area_of_triangle.js
@@ -0,0 +1,8 @@
+//Write a function that takes the base and height of a triangle and return its area.
+
+
+function triArea(base, height){
+    return base*height*0.5
+}
+
+console.log(triArea(15,8))
