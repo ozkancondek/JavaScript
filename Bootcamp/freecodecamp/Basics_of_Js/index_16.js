1294f91eccee417bda40b679a01b61fd697c4940
ozkancondek <ozkncndek@gmail.com>
2021-09-11 13:24:51 +0200

    while loop

---
+++ Bootcamp/freecodecamp/Basics_of_Js/index_16.js
@@ -0,0 +1,17 @@
+//While loop
+var ourArray = [];
+var i = 0;
+while (i < 5){
+    ourArray.push(i);
+    i++;
+}
+console.log(ourArray);
+
+var x = 0
+while (true){
+    console.log("Hello");
+    x++;
+    if (x == 4){
+        break
+    }
+}
