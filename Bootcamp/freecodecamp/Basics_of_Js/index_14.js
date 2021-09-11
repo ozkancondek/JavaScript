1e764c1c70e6e9fbdaca05ba925bb0c1e62cf9a7
ozkancondek <ozkncndek@gmail.com>
2021-09-11 12:54:50 +0200

    manupulating objects

---
+++ Bootcamp/freecodecamp/Basics_of_Js/index_14.js
@@ -0,0 +1,18 @@
+//Manupulating complex objects
+var ourMusic = [    // --> This is an array which contains one object inside. 
+    {
+      "artist": "Daft Punk",
+      "title": "Homework",
+      "release_year": 1997,
+      "formats": [ 
+        "CD", 
+        "Cassette", 
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+
+  console.log(typeof(ourMusic)); //--> object
+
+// JavaScript Object Notation or JSON is a related data interchange format used to store data.
