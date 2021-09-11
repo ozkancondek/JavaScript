d0e670ef9f6a23c3099ce39d5589043d4a59a70f
ozkancondek <ozkncndek@gmail.com>
2021-09-11 16:10:48 +0200

    parseInt(str,numberBase)

---
+++ Bootcamp/freecodecamp/Basics_of_Js/index_21.js
@@ -0,0 +1,9 @@
+//Use the parseInt Function
+
+var a;
+a = parseInt("0065");
+console.log(a); // 65
+
+//Use the parseInt Function with a Radix
+var b = parseInt("101011",2);  //retuens from binary to decimal
+console.log(b); // equal to 43
