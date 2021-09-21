b526ee63aedd87e24859688c303396b9ea244f4e
ozkancondek <ozkncndek@gmail.com>
2021-09-21 17:01:54 +0200

    add up numbers example added

---
+++ myProjects/level_1/add_numbers.js
@@ -0,0 +1,14 @@
+//https://edabit.com/challenge/4gzDuDkompAqujpRi
+
+function add(n){
+    let total = 0;
+    for (var i = 1; i <= n; i++){
+         
+        total = total + i;
+       
+
+    }
+    return total;
+}
+
+console.log(add(600));
