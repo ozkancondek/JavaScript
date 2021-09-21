2819e4c353ca52cbd6121165eb867a565ab36e30
ozkancondek <ozkncndek@gmail.com>
2021-09-21 17:23:56 +0200

    matcHouse example added

---
+++ myProjects/level_1/matchstick_houses.js
@@ -0,0 +1,18 @@
+ //https://edabit.com/challenge/tYHkTdFrEmWfxpPKF
+
+ 
+
+ function matcHouse(n){
+  
+     if (n==0){
+         return 0;
+     }else{
+        return 6+ (n-1)*5
+     }
+     
+     
+     
+ }
+         
+ console.log(matcHouse(1));
+ 
