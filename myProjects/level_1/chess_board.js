a7bdd8301cb15d1a5bebf5bcdae9f6d66dfe6b89
ozkancondek <ozkncndek@gmail.com>
2021-09-27 23:59:28 +0200

    chess board modified

--- myProjects/level_1/chess_board.js
+++ myProjects/level_1/chess_board.js
@@ -3,8 +3,21 @@
 //https://edabit.com/challenge/kDoi6Eob7KuELwjjp
 
 
-const chessBoard = ([file, rank]) => (
+/* const chessBoard = ([file, rank]) => (
 	(file.charCodeAt() % 2 === rank % 2) ? 'black' : 'white'
 );
 
-console.log(chessBoard("a1"))
+console.log(chessBoard("a1")) */
+
+
+
+const chessBoard = function(code){
+    let letter = "aceg"
+    let letter2 = "bdfh"
+    for (var i = 0; i<8; i++){
+        for( var j = 0; j<8; j++){
+            
+
+        }
+    }
+}
