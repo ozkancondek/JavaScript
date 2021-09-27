bcd33c861d9f4584907dd2d62a54df01f34678cd
ozkancondek <ozkncndek@gmail.com>
2021-09-28 00:17:01 +0200

    chess board example modified

--- myProjects/level_1/chess_board.js
+++ myProjects/level_1/chess_board.js
@@ -12,12 +12,21 @@ console.log(chessBoard("a1")) */
 
 
 const chessBoard = function(code){
-    let letter = "aceg"
-    let letter2 = "bdfh"
-    for (var i = 0; i<8; i++){
-        for( var j = 0; j<8; j++){
-            
-
+    let letter = "abcdefgh"
+    for (var i = 0; i < letter.length; i++){
+        if((letter.indexOf(code[i]) % 2 == 0 && parseInt(code[1]) %2 == 1)){
+            return "black"
+        }else{
+            return "white"
         }
     }
+
 }
+
+console.log(chessBoard("d1"))
+
+
+/*
+var c = "ozkan".includes("a")
+console.log(c)   return boolean value
+*/ 
