8fde481deb48c60b9dac057625816e8163432fa0
ozkancondek <ozkncndek@gmail.com>
2021-09-29 17:55:23 +0200

    find vowels in string example added

---
+++ Bootcamp/freecodecamp/Regular_Expressions/example_1.js
@@ -0,0 +1,7 @@
+// => all the vowels in string
+
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi;  
+let result =quoteSample.match(vowelRegex);  
+console.log(result);
