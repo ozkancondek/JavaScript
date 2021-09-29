8481a06f3afeade8625ccbec54e5de77e8c1c07b
ozkancondek <ozkncndek@gmail.com>
2021-09-29 17:29:55 +0200

    i flag

---
+++ Bootcamp/freecodecamp/Regular_Expressions/index_2.js
@@ -0,0 +1,10 @@
+//Ignore Case While Matching--i flag(ignore)
+
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; 
+let result = fccRegex.test(myString);
+console.log(result); //returns true
+
+
+ 
