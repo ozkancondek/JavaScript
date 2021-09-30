ede5f75af2b3c10ec646e8baa2e1666f9c763b44
ozkancondek <ozkncndek@gmail.com>
2021-09-30 20:17:04 +0200

    function factory example added

---
+++ myProjects/level_1/function_factory.js
@@ -0,0 +1,16 @@
+
+
+
+
+const plusFive = function(baseNum){
+    return function(x){
+        return baseNUm + x;
+        // or return (x) => baseNUm + x;
+    }
+     
+}
+
+ 
+
+
+//const makePlusFunction = baseNum => num => baseNum + num;
