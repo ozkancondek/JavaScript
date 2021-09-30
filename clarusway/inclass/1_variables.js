92a17953c55e89d327c75376673436ee723ba207
ozkancondek <ozkncndek@gmail.com>
2021-09-30 21:03:57 +0200

    inclass variables

---
+++ clarusway/inclass/1_variables.js
@@ -0,0 +1,15 @@
+//var let const
+
+
+
+var a = 10;
+console.log("Value of a =",a);
+{
+    let b = 20;
+    console.log("Value of b =",b);
+}
+
+console.log(a);
+//console.log(b);   this statement gonna give error because i can use b value in scoped
+
+
