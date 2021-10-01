9e7ba69fb7292b048990e713825ae40dae1e3f58
ozkancondek <ozkncndek@gmail.com>
2021-10-01 07:10:50 +0200

    seven boom example added

---
+++ myProjects/level_2/seven_boom.js
@@ -0,0 +1,9 @@
+
+
+//https://edabit.com/challenge/6R6gReGTGwzpwuffD
+
+
+const sevenBoom = (arr) => /7/.test(arr)==true ? "Boom!":"There is no 7 in the array."
+     
+
+console.log(sevenBoom( [8, 6, 33, 100]));
