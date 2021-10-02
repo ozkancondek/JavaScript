54ab1de44b672b0f56b96b3d22e0a61aa7c3e4f9
ozkancondek <ozkncndek@gmail.com>
2021-10-02 15:49:49 +0200

    js operators

---
+++ clarusway/inclass/3_operators.js
@@ -0,0 +1,17 @@
+var a = 3;
+var b = a--; // post increment. 
+var c = "ozkan";
+var x = "a"
+var f = "7"
+var e = "2"
+var d = a+c;
+console.log(d); // sum of str + number 
+console.log(typeof d); // str
+console.log(a);
+console.log(f*e);
+console.log(c*f); //NaN
+console.log(a*c); //NaN
+console.log(typeof f*e); //  NaN
+console.log("a".charCodeAt()); //order of a
+console.log(015+025); // 34 octadecimal
+console.log(015+25); //38
