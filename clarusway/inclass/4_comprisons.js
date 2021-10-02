c92acc8beac5726d0795896e222b8adbfd18c151
ozkancondek <ozkncndek@gmail.com>
2021-10-02 16:39:45 +0200

    comprison operators added

---
+++ clarusway/inclass/4_comprisons.js
@@ -0,0 +1,58 @@
+console.log(9 < 8 ); //false
+console.log(9 < 8 < 7 ); //true
+console.log(false < 7 ); //true
+console.log(false < -1); //false
+console.log(false > -1); //true
+console.log(null+3); //3
+console.log(true > false); //true */
+/* console.log('false equals false, correct? ', false == false);
+console.log('Boolean(0) equals FALSE, correct? ', Boolean(0) == false);
+console.log('0 equals false, correct? ', 0 == false);
+console.log('Boolean("0") equals TRUE, correct? ', Boolean('0') == true);
+console.log('0' == false); */
+/* console.log('\n*** null boolean operations ***');
+// console.log('Boolean(null) equals to', Boolean(null)); 
+// console.log('null == false equals to', null == false);
+// console.log('null == true equals to', null == true); 
+// console.log('null == null equals to', null == null); 
+// console.log('typeof null equals to', typeof null); 
+// console.log('0 == null equals to', 0 == null); // 
+// console.log('Boolean(null) == Boolean(0)', Boolean(null) == Boolean(0)); */
+/* console.log('\n*** undefined boolean operations ***');
+console.log('Boolean(undefined) equals', Boolean(undefined));
+console.log('undefined == false correct?', undefined == false);
+console.log('undefined == true correct?', undefined == true);
+console.log('0 == undefined correct?', 0 == undefined);
+console.log('null == undefined correct?', null == undefined);
+console.log('null === undefined correct?', null === undefined); */
+// console.log('\n*** NaN operations ***');
+// console.log(NaN == NaN); 
+// console.log(NaN === NaN); 
+// console.log(typeof NaN);
+// ! use global isNaN() function to overcome this
+/* var a;
+while (isNaN(a)){
+    a = prompt("Enter a Number")
+    a = Number(a)
+    console.log(a, typeof a)
+} */
+/* console.log(false && true);
+console.log( 2 && 0);
+console.log(0 && 3);
+console.log(2 && 3); */
+// console.log(2 && 3 && 4 );
+// OR
+// console.log((8>2) || 3)
+// console.log(0 || 3)
+//Not
+// console.log(!true);
+// console.log(!false);
+// console.log(!5);
+// console.log(!0);
+//Nullish
+/* var n;
+console.log(n);
+// console.log(null ?? true);
+// console.log((8>9) ?? 0);
+console.log(n ?? 100);
+console.log(100 ?? n); */
