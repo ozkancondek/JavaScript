9a7d560ca674bba01608d5da6dbad912d9d87fbe
ozkancondek <ozkncndek@gmail.com>
2021-10-03 22:41:41 +0200

    array properties

--- Bootcamp/freecodecamp/Data_structure/index_1.js
+++ Bootcamp/freecodecamp/Data_structure/index_1.js
@@ -1,1 +1,28 @@
+//complex arrays
 
+let complexArray = [
+    [
+      {
+        one: 1,
+        two: 2
+      },
+      {
+        three: 3,
+        four: 4
+      }
+    ],
+    [
+      {
+        a: "a",
+        b: "b"
+      },
+      {
+        c: "c",
+        d: "d"
+      }
+    ]
+  ];
+console.log(complexArray);
+  // i can also modify the items
+  complexArray[0] = "ozkan";
+  console.log(complexArray);
