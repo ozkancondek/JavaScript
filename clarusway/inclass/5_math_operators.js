48a613cb64e255014df88d6a97d9bf76bcfee95c
ozkancondek <ozkncndek@gmail.com>
2021-10-04 18:59:36 +0200

    math operators

---
+++ clarusway/inclass/5_math_operators.js
@@ -0,0 +1,19 @@
+/*
+console.log('\n*** Some builtin Math functions ***');
+console.log('Math.floor(10.7)', Math.floor(10.7));
+console.log('Math.floor(10.3)', Math.floor(10.3));
+console.log('Math.floor(-10.7)', Math.floor(-10.7));
+console.log('Math.floor(-10.3)', Math.floor(-10.3));
+console.log('Math.ceil(10.7)', Math.ceil(10.7));
+console.log('Math.ceil(10.3)', Math.ceil(10.3));
+console.log('Math.ceil(-10.7)', Math.ceil(-10.7));
+console.log('Math.ceil(-10.3)', Math.ceil(-10.3));
+console.log('Math.trunc(10.7)', Math.trunc(10.7));
+console.log('Math.trunc(10.3)', Math.trunc(10.3));
+console.log('Math.trunc(-10.7)', Math.trunc(-10.7));
+console.log('Math.trunc(-10.3)', Math.trunc(-10.3));
+console.log('Math.round(10.7)', Math.round(10.7));
+console.log('Math.round(10.3)', Math.round(10.3));
+console.log('Math.round(-10.7)', Math.round(-10.7));
+console.log('Math.round(-10.3)', Math.round(-10.3));
+console.log('Math.random()', Math.random()); */
