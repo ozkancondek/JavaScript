d2150770eb7f0a1834503fc7d605461d1e392bb7
ozkancondek <ozkncndek@gmail.com>
2021-10-05 23:56:37 +0200

    complex array

---
+++ Bootcamp/freecodecamp/Data_structure/index_5.js
@@ -0,0 +1,22 @@
+//Create complex multi-dimensional arrays
+
+
+
+let nestedArray = [
+    ['deep'],
+    [
+      ['deeper'], ['deeper'] 
+    ],
+    [
+      [
+        ['deepest'], ['deepest']
+      ],
+      [
+        [
+          ['deepest-est?']
+        ]
+      ]
+    ]
+  ];
+
+  console.log(nestedArray[2][1][0][0][0]);
