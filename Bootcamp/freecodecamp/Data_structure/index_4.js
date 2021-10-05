3a7e9dea26de6f9f18eaf693b0735e923b76df96
ozkancondek <ozkncndek@gmail.com>
2021-10-05 23:45:48 +0200

    indexOf()

---
+++ Bootcamp/freecodecamp/Data_structure/index_4.js
@@ -0,0 +1,14 @@
+// => Check For The Presence of an Element With indexOf()
+
+// when called, it returns the position, 
+//or index, of that element, or -1 if the element does not exist on the array.
+
+
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+
+fruits.indexOf('dates');
+fruits.indexOf('oranges');
+fruits.indexOf('pears');
+
+
+ 
