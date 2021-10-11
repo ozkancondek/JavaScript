a3367d03553af3056318212500f1aa850cfc1c86
ozkancondek <ozkncndek@gmail.com>
2021-10-11 23:39:07 +0200

    map function

---
+++ Bootcamp/freecodecamp/Functional_programming/index_5.js
@@ -0,0 +1,23 @@
+//Implement map on a Prototype
+
+
+// The global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+
+   
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+   
+
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+console.log(new_s);
