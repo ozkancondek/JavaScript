8d577b8858d3ab495e80306b3b675314a6155748
ozkancondek <ozkncndek@gmail.com>
2021-10-12 00:35:25 +0200

    concat method

---
+++ Bootcamp/freecodecamp/Functional_programming/index_7.js
@@ -0,0 +1,12 @@
+//Combine Two Arrays Using the concat Method
+
+
+let a = [1, 2, 3].concat([4, 5, 6]);
+console.log(a);
+
+//=> Add Elements to the End of an Array Using concat Instead of push
+
+var arr = [1, 2, 3];
+arr.push([4, 5, 6]);
+ 
+console.log( arr.concat(8)); // or concat([8])
