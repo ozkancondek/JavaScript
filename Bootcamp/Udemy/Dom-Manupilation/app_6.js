91957855494db781d03a9b2ebf29a729b063406b
ozkancondek <ozkncndek@gmail.com>
2021-10-16 02:45:33 +0200

    delet ean element

---
+++ Bootcamp/Udemy/Dom-Manupilation/app_6.js
@@ -0,0 +1,17 @@
+// Delete an element
+// I am gonna get ul
+
+const todoList = document.querySelector("ul.list-group");
+const todos = document.querySelectorAll("li.list-group-item");
+
+//remove method
+
+//todos[0].remove(); // delete todo1
+
+//removechild
+
+//todoList.removeChild(todoList.lastElementChild) // delete last child 
+//todoList.removeChild(todos[3]); also the same code line
+
+
+console.log(todos);
