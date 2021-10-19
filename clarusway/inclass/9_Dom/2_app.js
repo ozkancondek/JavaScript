93805f14d4be2bb6e22fdca6e1196ae25a2de452
ozkancondek <ozkncndek@gmail.com>
2021-10-19 21:10:00 +0200

    lesson cover

--- clarusway/inclass/9_Dom/2_app.js
+++ clarusway/inclass/9_Dom/2_app.js
@@ -49,7 +49,7 @@ lastName.value = "condek"
 -full name = FIRST LAST
 -email = first.last@gmail.com
 
- */
+
 
 let firstname = document.getElementById("first_name");
 let lastname = document.getElementById("last_name");
@@ -60,7 +60,65 @@ firstname.value = "ozkan";
 lastname.value = "condek";
 fullname.value = `${firstname.value.toUpperCase()} ${lastname.value.toUpperCase()}`; 
 email.value =   `${firstname.value.toLowerCase()}${lastname.value.toLowerCase()}@gmail.com`;
+ */
 
+//innertext, innerHTML, textcontent
+// for header div
+
+let headerdiv = document.getElementById("headerId"); // get object first
+let inner = headerdiv.innerHTML; // ican display this div with elements (also tags)
+let text = headerdiv.innerText; // only text
+let content = headerdiv.textContent; // gets also spaces
+
+//add logeo with inner html
+//headerdiv.innerText = "ozkan" //adds text
+//headerdiv.innerHTML = `add image tag` // but deletes all child then adds this. to prevent it
+//headerdiv.innerHTML += `add image tag`
+
+//create an element
+
+/* let element1 = document.createElement("img"); // i am creating with tag
+element1.setAttribute("src","filepath");
+element1.setAttribute("alt","ozkan")
+element1.style.width = "50px"
+element1.style.height = "50px"//set attributes
+element1.className = "headerLogo"; // get features of headerlogo 
+element1.classList.add("headerclass") // add a new class to classlist
+console.log(elemnt1.classList); // list of classes
+element1.classList.remove("leaderLogo"); //remove class
+headerdiv.appendChild(element1) //headerdiv.innerHTML += element1 */
+
+//add paths to web page
+
+let pathlist = ["Fullstack","AWS","Data Science","Cyber Security"];
+
+//get selectbox first and add option
+
+let path = document.querySelector("#paths");
+
+/* for(let i = 0; i < pathlist.length; i++){
+    path.innerHTML += `<option>${pathlist[i]}</option>`
+} */
+
+//for of
+/* for(i of pathlist){
+    path.innerHTML += `<option>${i}</option>`
+}
+ */
+//for each
+
+pathlist.forEach(el => {
+    path.innerHTML += `<option>${el}</option>`
+})
+
+
+
+
+
+
+ 
+ 
+ 
 
  
 
