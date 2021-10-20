b88aef8cdb4d5896d059f4e6fcd34da390a30967
ozkancondek <ozkncndek@gmail.com>
2021-10-20 04:39:58 +0200

    spread operator

---
+++ Bootcamp/Udemy/ES6/spread_operator.js
@@ -0,0 +1,19 @@
+
+
+
+const arr = [3,5,0,7];
+
+//console.log(...arr);
+
+//const arr1 = ["password",...arr]
+
+//console.log(arr1);
+
+//const [a,b,...rest] = arr; //a = 3, b = 5 ---- rest of the array
+
+//console.log(rest);
+
+
+//const addNumbers = (a,b,c) => console.log(a+b+c);
+
+//addNumbers(...arr); // 3+5+0
