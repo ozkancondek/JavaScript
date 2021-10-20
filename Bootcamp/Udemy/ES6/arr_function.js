ef88c2f8ddc44cecd6ca14832eec5520510a5d4f
ozkancondek <ozkncndek@gmail.com>
2021-10-20 04:23:36 +0200

    arrow function

---
+++ Bootcamp/Udemy/ES6/arr_function.js
@@ -0,0 +1,13 @@
+/* const hello = function(){
+    console.log("hello");
+}
+ */
+const hello= () => {console.log("hello")};
+
+hello();
+
+const square = x => x**2; // if there is only one transaction i dont need return
+
+console.log(square(8));
+
+const sum = (a,b) => a+b;
