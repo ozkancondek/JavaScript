e496dcdf20040c10b20ccf70b385a905dc384c5e
ozkancondek <ozkncndek@gmail.com>
2021-11-14 10:45:34 +0100

    async

---
+++ clarusway/inclass/18_async/1_async.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+//sycnc example
+
+
+// console.log("hello");
+// alert("waits until submit");
+// console.log("world");
+
+
+
+/* const wait = function () {
+    setTimeout(() => console.log("wait"), 2000)
+}
+
+console.log("hello");
+wait(1000);
+console.log("world"); */
+
+
+
+
+
+
+//async
+
+const wait = function () {
+    setTimeout(() => console.log("wait"), 2000)
+}
+
+console.log("hello");
+wait(1000);
+console.log("world");
+
+
+
+
+
