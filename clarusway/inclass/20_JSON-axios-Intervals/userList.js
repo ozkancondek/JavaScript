37de6f3afefcd119006558f94f6c97fe05d74068
ozkancondek <ozkncndek@gmail.com>
2021-11-25 16:22:09 +0100

    user List files

---
+++ clarusway/inclass/20_JSON-axios-Intervals/userList.js
@@ -0,0 +1,44 @@
+//get user data from api
+
+const tbody = document.getElementById("tbodyUserList");
+window.onload = () => {
+  getApiUserList();
+  setInterval(getApiUserList, 5000);
+};
+const getApiUserList = async (pageNo) => {
+  tbody.innerHTML = ""; // unload page before every call of api
+  showLoading();
+  // const responseData = await axios({
+  //     url: "https://reqres.in/api/users?page=1",
+  //     method: "get"
+  // });
+
+  const responseData = await axios.get("https://reqres.in/api/users?page=2");
+
+  // console.log(responseData.data.data);
+  if (responseData.data.data[0].id == undefined) {
+    alert("userlist not found!");
+    removeLoading();
+  } else {
+    for (let i = 0; i < responseData.data.data.length; i++) {
+      tbody.innerHTML += ` <tr>
+            <td>
+            <img src="${responseData.data.data[i].avatar}"/>
+            </td>
+            <td>
+            ${responseData.data.data[i].id}
+            </td>
+            <td>
+            ${responseData.data.data[i].email}
+            </td>
+            <td>
+            ${responseData.data.data[i].first_name}
+            </td>
+            <td>
+            ${responseData.data.data[i].last_name}
+            </td>
+     </tr>`;
+      removeLoading();
+    }
+  }
+};
