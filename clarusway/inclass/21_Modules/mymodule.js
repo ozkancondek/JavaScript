0a098a4fb4adb31eadc2468d108864fce4393505
ozkancondek <ozkncndek@gmail.com>
2021-11-29 20:41:17 +0100

    modules and bable

--- clarusway/inclass/21_Modules/mymodule.js
+++ clarusway/inclass/21_Modules/mymodule.js
@@ -2,7 +2,7 @@ function add(n1, n2) {
   return n1 + n2;
 }
 
-const WORKING_HOURS_WEEK = 40;
+const WORKING_HOURS_WEEK = 40; // 45;
 
 let counter = 0;
 
@@ -16,6 +16,16 @@ function dec(amount = 1) {
   return counter;
 }
 
+export default function (num) {
+  return num * num;
+}
+
 let localCounter = 5;
 
-const moduleName = "This is my module 1";
+const moduleName = 'mymodule 1';
+// export default moduleName;
+// export default 'mymodule 1'
+
+export { WORKING_HOURS_WEEK, WORKING_HOURS_WEEK as WHW, add as sum, counter, dec, inc };
+
+console.log(`${moduleName} is loaded`);
